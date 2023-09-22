a789fafe72332b1f5830304bc6272c1c9b10c1f5
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-09-22 11:09:45 +0100

    added comments

--- json_session/json_value_from_key_input.py
+++ json_session/json_value_from_key_input.py
@@ -3,8 +3,10 @@ import json
 # reads in json
 parsed_json = json.loads(open('example_json.json').read())
 
+# asks for desired key
 key = input("Please input key: ")
 
+# if the key is in the json, it will return the associated value, if not it will tell you key wasnt found
 if key not in parsed_json:
     print("Key not found...")
 else:
