1f856985ef6bb5b401cc660dd9ea65963b6c5c31
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-09-22 12:17:02 +0100

    Added yaml checker and yaml to json scripts

---
+++ json_session/check_yaml.py
@@ -0,0 +1,24 @@
+import yaml
+import os
+import sys
+
+# checks if arguments are given
+if len(sys.argv) > 1:
+    # checks if the file is valid
+    if os.path.exists(sys.argv[1]):
+        # reads file
+        file = open(sys.argv[1], "r")
+
+        # use load instead of loads as we don't need to convert into dictionary, we're just checking if file is valid
+        yaml.safe_load(file)
+
+        # closes file
+        file.close()
+
+        # if no error occured, then json must be valid!
+        print("YAML is valid!")
+
+    else:
+        print(f"{sys.argv[1]} is not found")
+else:
+    print("Usage: check_yaml.py <file>")
